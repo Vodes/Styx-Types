aadeb00850626bed5cda85636cafca4aa87ee5b1
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-30 00:55:49 +0100

    Add some documentation for MediaWatched

--- src/commonMain/kotlin/moe/styx/types/Media.kt
+++ src/commonMain/kotlin/moe/styx/types/Media.kt
@@ -32,11 +32,35 @@ data class MediaInfo(
     var hasEnglishDub: Int, var hasGermanDub: Int, var hasGermanSub: Int
 )
 
+/**
+ * Class to save progress on an entry.
+ *
+ * @param entryID ID for the corresponding episode or movie
+ * @param userID ID for the user
+ * @param lastWatched The time this was last watched (not sure what I'm gonna use this for yet)
+ * @param progress Where the last playback session stopped, in seconds. This will be used to resume playback and can be reduced.
+ * @param progressPercent Where the last playback session stopped, in percent
+ * @param maxProgress The maximum progress reached so far, in percent. This will essentially mean that something has been watched if >80%. This can never be reduced.
+ */
 @Serializable
-data class MediaWatched(val entryID: String, val userID: String, var lastWatched: Long, var progress: Long, var progressPercent: Float, var maxProgress: Float)
+data class MediaWatched(
+    val entryID: String,
+    val userID: String,
+    var lastWatched: Long,
+    var progress: Long,
+    var progressPercent: Float,
+    var maxProgress: Float
+)
 
 @Serializable
-data class MediaSchedule(var mediaID: String, var day: ScheduleWeekday, var hour: Int, var minute: Int, var isEstimated: Int = 0, var finalEpisodeCount: Int = 0)
+data class MediaSchedule(
+    var mediaID: String,
+    var day: ScheduleWeekday,
+    var hour: Int,
+    var minute: Int,
+    var isEstimated: Int = 0,
+    var finalEpisodeCount: Int = 0
+)
 
 enum class ScheduleWeekday {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
